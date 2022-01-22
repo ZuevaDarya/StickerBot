d6dbad4d04de9cd4667ad997ff2d5697107baa67
ZuevaDarya <80260736+ZuevaDarya@users.noreply.github.com>
2022-01-22 13:51:15 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function () {
+    $('.slider__wrapper').cycle({
+        fx:'fade',
+        speed: 300,
+        timeout: 3000,
+        cleartypeNoBg: true,
+        pager: '#pager',
+        pause: 1,
+
+        pagerAnchorBuilder: function(idx, slide) {
+            return '<li><a href="#"></a></li>';
+        }
+    });
+ 
+    //Спойлеры
+    $('.instruction__item-title').click(function (event) {
+        $(this).toggleClass('active').next().slideToggle(300);
+        $(this).next().css('display', 'flex');
+    });
+
+    //Бургер
+    $('.header__burger').click(function(event){
+        $('.header__burger, .header__links').toggleClass('active-menu');
+    });
+});
+
+
+
